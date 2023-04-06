f7937f7020c10487354d3dfdf7a9b50d09aa0555
Pedoni <emanuele.lamagna@studio.unibo.it>
2023-04-06 18:59:01 +0200

    Added results popup functionality
    Now, after the image classification, you can analyze all the results, with the monuments with their probability percentage.

--- app/src/main/java/com/unibo/cesenamonuments/MainActivity.java
+++ app/src/main/java/com/unibo/cesenamonuments/MainActivity.java
@@ -9,8 +9,14 @@ import android.media.ThumbnailUtils;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -28,22 +34,46 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button camera, gallery;
+    Button camera;
+    Button gallery;
+    Button details;
     ImageView imageView;
     TextView result;
     final int imageSize = 224;
+    List<TextView> monuments;
+    float[] results;
+    View popupView;
+    final String[] classes = {
+        "Chiesa di San Giovanni Battista",
+        "Colonna dell'Ospitalità",
+        "Fontana Masini",
+        "Giardini Pubblici",
+        "Palazzo del Ridotto",
+        "Ponte Vecchio",
+        "Rocca Malatestiana",
+        "Teatro Bonci"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        monuments = new ArrayList<>();
         camera = findViewById(R.id.button);
         gallery = findViewById(R.id.button2);
+        details = findViewById(R.id.details);
         result = findViewById(R.id.result);
         imageView = findViewById(R.id.imageView);
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        popupView = inflater.inflate(R.layout.details, null);
+        addMonuments();
+
         camera.setOnClickListener(view -> {
             if (checkSelfPermission(android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
                 Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -52,9 +82,45 @@ public class MainActivity extends AppCompatActivity {
                 requestPermissions(new String[]{Manifest.permission.CAMERA}, 100);
             }
         });
+
         gallery.setOnClickListener(view -> {
             Intent cameraIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
             startActivityForResult(cameraIntent, 1);
+        });
+
+        details.setOnClickListener(view ->{
+            if (results != null) {
+                onButtonShowPopupWindowClick(view);
+            }
+        });
+    }
+
+    private void addMonuments() {
+        monuments.add(popupView.findViewById(R.id.sangiovanni));
+        monuments.add(popupView.findViewById(R.id.colonna));
+        monuments.add(popupView.findViewById(R.id.fontana));
+        monuments.add(popupView.findViewById(R.id.giardini));
+        monuments.add(popupView.findViewById(R.id.ridotto));
+        monuments.add(popupView.findViewById(R.id.ponte));
+        monuments.add(popupView.findViewById(R.id.rocca));
+        monuments.add(popupView.findViewById(R.id.bonci));
+    }
+
+    private void onButtonShowPopupWindowClick(View view) {
+        for(int i = 0; i < monuments.size(); i++) {
+            String text = classes[i] + ": ";
+            float value = results[i];
+            monuments.get(i).setText(text + (value < 0.01 ? "<0.01%" : Math.floor(value * 100) / 100 + "%"));
+        }
+
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true;
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+        popupView.setOnTouchListener((v, event) -> {
+            popupWindow.dismiss();
+            return true;
         });
     }
 
@@ -79,7 +145,7 @@ public class MainActivity extends AppCompatActivity {
         return maxPos;
     }
 
-    public void classifyImage(Bitmap image) {
+    private void classifyImage(Bitmap image) {
         ImageProcessor imageProcessor =
             new ImageProcessor.Builder()
                 .add(new ResizeOp(224, 224, ResizeOp.ResizeMethod.BILINEAR))
@@ -91,18 +157,9 @@ public class MainActivity extends AppCompatActivity {
         TensorBuffer probabilityBuffer = TensorBuffer.createFixedSize(new int[]{1, 8}, DataType.FLOAT32);
         try {
             new Interpreter(loadModelFile()).run(tImage.getBuffer(), probabilityBuffer.getBuffer());
-            String[] classes = {
-                "Chiesa di San Giovanni Battista",
-                "Colonna dell'Ospitalità",
-                "Fontana Masini",
-                "Giardini Pubblici",
-                "Palazzo del Ridotto",
-                "Ponte Vecchio",
-                "Rocca Malatestiana",
-                "Teatro Bonci"
-            };
             int maxPos = getMaxPosition(probabilityBuffer.getFloatArray());
             result.setText(classes[maxPos]);
+            results = probabilityBuffer.getFloatArray();
         } catch (IOException e) {
             e.printStackTrace();
         }
